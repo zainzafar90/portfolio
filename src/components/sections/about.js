9f901bc3370819067a81e5dc78f9ea4558cd09ae
Zain Zafar <zain.zafar@stoqo.com>
2020-05-06 16:57:00 +0500

    Open GitHub link in new tab on avatar click

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -129,7 +129,7 @@ const About = ({ data }) => {
           </SkillsContainer>
         </StyledContent>
         <StyledPic>
-          <StyledAvatarLink href={github}>
+          <StyledAvatarLink href={github} target="_blank" rel="nofollow noopener noreferrer">
             <StyledAvatar fluid={avatar.childImageSharp.fluid} alt="Avatar" />
           </StyledAvatarLink>
         </StyledPic>
